4efc5c17a41fe3cff0b81736f77d6a6801b95b27
Mike Zhong <mzhong@bluebirdbio.com>
2017-07-06 16:55:51 -0400

    '7.6.17'

--- catan/game/sim.py
+++ catan/game/sim.py
@@ -5,9 +5,9 @@ django.setup()
 
 import catan.models as catan
 import random
+import numpy as np
 
 random.seed = 0
-
 
 class Game():
     def __init__(self, sim=True):
@@ -18,6 +18,7 @@ class Game():
             self.resources = None
             self.winner = False
             self.initialized = False
+            self.board = np.zeros((55, 55))
 
         else:
             pass
@@ -50,6 +51,81 @@ class Game():
             tile.save()
 
         self.initialized = True
+
+    def init_board(self):
+        self.board[1, 4] = 1
+        self.board[1, 5] = 2
+        self.board[2, 5] = 3
+        self.board[2, 6] = 4
+        self.board[3, 6] = 5
+        self.board[3, 7] = 6
+        self.board[4, 8] = 7
+        self.board[5, 9] = 8
+        self.board[6, 10] = 9
+        self.board[7, 11] = 10
+        self.board[8, 13] = 11
+        self.board[9, 13] = 12
+        self.board[9, 14] = 13
+        self.board[10, 14] = 14
+        self.board[10, 15] = 15
+        self.board[11, 15] = 16
+        self.board[8, 12] = 17
+        self.board[11, 16] = 18
+        self.board[12, 17] = 19
+        self.board[13, 18] = 20
+        self.board[14, 19] = 21
+        self.board[15, 20] = 22
+        self.board[16, 21] = 23
+        self.board[17, 23] = 24
+        self.board[18, 23] = 25
+        self.board[18, 24] = 26
+        self.board[19, 24] = 27
+        self.board[19, 25] = 28
+        self.board[20, 25] = 29
+        self.board[20, 26] = 30
+        self.board[21, 26] = 31
+        self.board[17, 22] = 32
+        self.board[21, 27] = 33
+        self.board[22, 28] = 34
+        self.board[23, 29] = 35
+        self.board[24, 30] = 36
+        self.board[25, 31] = 37
+        self.board[26, 32] = 38
+        self.board[27, 33] = 39
+        self.board[28, 34] = 40
+        self.board[29, 34] = 41
+        self.board[29, 35] = 42
+        self.board[30, 35] = 43
+        self.board[30, 36] = 44
+        self.board[31, 36] = 45
+        self.board[31, 37] = 46
+        self.board[32, 37] = 47
+        self.board[32, 38] = 48
+        self.board[33, 38] = 49
+        self.board[34, 39] = 50
+        self.board[35, 40] = 51
+        self.board[36, 41] = 52
+        self.board[37, 42] = 53
+        self.board[38, 43] = 54
+        self.board[39, 44] = 55
+        self.board[40, 44] = 56
+        self.board[40, 45] = 57
+        self.board[41, 45] = 58
+        self.board[41, 46] = 59
+        self.board[42, 46] = 60
+        self.board[42, 47] = 61
+        self.board[43, 47] = 62
+        self.board[44, 48] = 63
+        self.board[45, 49] = 64
+        self.board[46, 50] = 65
+        self.board[47, 51] = 66
+        self.board[48, 52] = 67
+        self.board[49, 52] = 68
+        self.board[]
+
+        self.board += self.board.transpose()
+
+
 
     def roll_dice(self):
         return random.randint(1,6) + random.randint(1,6)
@@ -88,6 +164,8 @@ if __name__ == "__main__":
         sim.initialize()
 
     turn = 0
+
+    print(sim.board.shape)
 
     while sim.winner == False:
         #sim.winner = True
